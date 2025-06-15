89696e98e3abf13c4e9fa480ae0185c40f832918
Prabin-sc1 <prabin.sc1@gmail.com>
2025-06-15 23:51:46 +0545

    feat: CRUD for Food entity completed

--- src/main/java/np/prabinsoft/Food/Delivery/service/FoodService.java
+++ src/main/java/np/prabinsoft/Food/Delivery/service/FoodService.java
@@ -4,10 +4,21 @@ import np.prabinsoft.Food.Delivery.io.FoodRequest;
 import np.prabinsoft.Food.Delivery.io.FoodResponse;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface FoodService {
 
     String uploadFile(MultipartFile file);
 
     FoodResponse addFood(FoodRequest foodRequest, MultipartFile file);
+
+    List<FoodResponse> getAllFood();
+
+    FoodResponse getFood(String id);
+
+    boolean deleteFile(String fileName);
+
+    void deleteFood(String id);
+
 
 }
